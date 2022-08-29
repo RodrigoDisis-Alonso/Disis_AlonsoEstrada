f676ce1ce295665c9b6eb3889a8f8ad5b9dbc252
erikfer201 <fernandoestradacruz41@gmail.com>
2022-08-28 21:03:28 -0500

    "Subir carpeta"

---
+++ Empleado.java
@@ -0,0 +1,70 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Empleado {
+
+    /**
+     * Default constructor
+     */
+    public Empleado() {
+    }
+
+    /**
+     * 
+     */
+    protected void Codigo_Empleado;
+
+    /**
+     * 
+     */
+    public void Nombre_Empleado;
+
+    /**
+     * 
+     */
+    public void Telefono;
+
+    /**
+     * 
+     */
+    public void Correo;
+
+    /**
+     * 
+     */
+    public void Area;
+
+
+
+    /**
+     * 
+     */
+    public void Adicionar() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Consultar1() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Eliminar() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Modificar() {
+        // TODO implement here
+    }
+
+}
